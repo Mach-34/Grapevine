d4271f5136d066e30fa4e88fdbb81763927d4ee6
Jack Gilcrest <me@jp4g.llc>
2024-03-28 03:47:56 -0600

    Updated build script to default to remote server

--- crates/grapevine_cli/build.rs
+++ crates/grapevine_cli/build.rs
@@ -5,6 +5,6 @@ fn main() {
     dotenv().ok();
 
     // try to load from env
-    let server_url = env::var("GRAPEVINE_SERVER").unwrap_or("http://localhost:8000".to_string());
+    let server_url = env::var("GRAPEVINE_SERVER").unwrap_or("https://grapevine.mach34.space".to_string());
     println!("cargo:rustc-env=SERVER_URL={}", server_url);
 }
