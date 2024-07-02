97e6fdf69cd9a383b5e26c034c5c466146fbf31d
Jack Gilcrest <me@jp4g.llc>
2024-07-02 04:44:33 +0300

    nuked routes, added identity proof/ user creation route

--- crates/grapevine_server/src/routes/mod.rs
+++ crates/grapevine_server/src/routes/mod.rs
@@ -1,28 +1,28 @@
 use lazy_static::lazy_static;
 use rocket::route::Route;
 mod proof;
-mod user;
+// mod user;
 
 lazy_static! {
     pub(crate) static ref USER_ROUTES: Vec<Route> = routes![
-        user::create_user,
-        user::add_relationship,
-        user::reject_pending_relationship,
-        user::get_pending_relationships,
-        user::get_active_relationships,
-        user::get_account_details,
-        user::get_user,
-        user::get_nonce,
-        user::get_pubkey,
-        user::get_all_degrees
+        // user::create_user,
+        // user::add_relationship,
+        // user::reject_pending_relationship,
+        // user::get_pending_relationships,
+        // user::get_active_relationships,
+        // user::get_account_details,
+        // user::get_user,
+        // user::get_nonce,
+        // user::get_pubkey,
+        // user::get_all_degrees
     ];
     pub(crate) static ref PROOF_ROUTES: Vec<Route> = routes![
-        proof::prove_phrase,
-        proof::degree_proof,
-        proof::get_available_proofs,
-        proof::get_phrase_connections,
-        proof::get_proof_with_params,
-        proof::get_known_phrases,
-        proof::get_phrase
+        proof::prove_identity,
+        // proof::degree_proof,
+        // proof::get_available_proofs,
+        // proof::get_phrase_connections,
+        // proof::get_proof_with_params,
+        // proof::get_known_phrases,
+        // proof::get_phrase
     ];
 }
